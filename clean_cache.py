774ee1c3f102ca08f2823836437d9602890d93e1
AEnjoy <964728598@qq.com>
2020-08-15 23:50:32 +0800

    Clean Cache Update

--- clean_cache.py
+++ clean_cache.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import os,platform
 print('开始清理缓存')
+os.rmdir('__pycache__')
 if platform.system()=='Windows':
     os.system('rd /s /q META-INF output rom system system_') 
     os.system('del /f /s /q *.img payload.bin system.new.dat.br system.transfer.list system.new.dat')
